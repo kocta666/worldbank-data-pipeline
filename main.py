730fe7a40bd6682854c45f1d08b7a39267eb7270
kocta <1kocta47@gmail.com>
2026-02-07 12:15:55 +0300

    main.py Добавлена обработка ошибок в fetch_worldbank_data

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ import requests
 import json
 import boto3
 
+# CONSTANTS
+WORLDBANK_API_URL = "https://api.worldbank.org/v2/country/US/indicator/SP.POP.TOTL?format=json"
+DEFAULT_FILENAME = "data.json"
+S3_ENDPOINT = "http://127.0.0.1:9000"
+S3_ACCESS_KEY = "minioadmin"
+S3_SECRET_KEY = "minioadmin"
+S3_REGION = "us-east-1"
+S3_BUCKET = "api-data"
+S3_OBJECT_NAME = "data.json"
+
 def fetch_worldbank_data():
-    url = "https://api.worldbank.org/v2/country/US/indicator/SP.POP.TOTL?format=json"
-    response = requests.get(url) # дергаем API
-    response.raise_for_status() # упадет если 4хх/5хх
-    return response.json() # получаем python-объект (list/dict)
+    try:
+        url = WORLDBANK_API_URL
+        response = requests.get(url) # дергаем API
+        response.raise_for_status() # упадет если 4хх/5хх
+        return response.json() # получаем python-объект (list/dict)
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе к WorldBank API: {e}")
+        raise # Пробрасываем дальше, если можно вернуть None
 
 
-def save_to_file(data, filename="data.json"):
+def save_to_file(data, filename=DEFAULT_FILENAME):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 def create_s3_client():
     return boto3.client("s3",
-                        endpoint_url="http://127.0.0.1:9000",
-                        aws_access_key_id="minioadmin",
-                        aws_secret_access_key="minioadmin",
-                        region_name="us-east-1",
+                        endpoint_url=S3_ENDPOINT,
+                        aws_access_key_id=S3_ACCESS_KEY,
+                        aws_secret_access_key=S3_SECRET_KEY,
+                        region_name=S3_REGION,
                         )
 
 def upload_to_s3(s3_client, bucket, filename, object_name):
@@ -27,14 +41,14 @@ def upload_to_s3(s3_client, bucket, filename, object_name):
 
 def main():
     data = fetch_worldbank_data()
-    save_to_file(data, "data.json")
+    save_to_file(data, DEFAULT_FILENAME)
 
     s3 = create_s3_client()
     upload_to_s3(
         s3_client=s3,
-        bucket="api-data",
-        filename="data.json",
-        object_name="data.json"
+        bucket=S3_BUCKET,
+        filename=DEFAULT_FILENAME,
+        object_name=S3_OBJECT_NAME
     )
 
 if __name__ == "__main__":
